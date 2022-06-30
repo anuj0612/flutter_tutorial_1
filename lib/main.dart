5cf1443d44713b23d5ce0e74405091a1d2d5593a
Yash Thakur <45893052+anuj0612@users.noreply.github.com>
2022-06-30 13:22:20 +0530

    flutter tutorial one done

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        title: const Text('I am rich'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body:const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+      ),
+    ),
+  ));
+}
